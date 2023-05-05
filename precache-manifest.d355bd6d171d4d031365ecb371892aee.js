df309f067f0264e53b8db5b32adda7033efec62c
Labranche <slabranche@cmontmorency.qc.ca>
2023-05-04 22:41:12 -0400

    Updates

---
+++ precache-manifest.d355bd6d171d4d031365ecb371892aee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e689bf2668d329ba93e30db4a04ef482",
+    "url": "/deploiement/index.html"
+  },
+  {
+    "revision": "89b8d0704f89e734a056",
+    "url": "/deploiement/static/css/main.f1d86555.chunk.css"
+  },
+  {
+    "revision": "7fbb420e4c602e6588fd",
+    "url": "/deploiement/static/js/2.ace05756.chunk.js"
+  },
+  {
+    "revision": "89b8d0704f89e734a056",
+    "url": "/deploiement/static/js/main.117d5c0c.chunk.js"
+  },
+  {
+    "revision": "bd7e56c0c1e6c34abf47",
+    "url": "/deploiement/static/js/runtime-main.02f24607.js"
+  }
+]);
