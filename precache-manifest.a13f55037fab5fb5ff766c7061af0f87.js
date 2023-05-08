af60502f36d665f50c1e855b724e6d887fd6af33
Sylvain Labranche <slabranche@cmontmorency.qc.ca>
2023-05-08 08:33:26 -0400

    Updates

--- precache-manifest.a13f55037fab5fb5ff766c7061af0f87.js
+++ precache-manifest.a13f55037fab5fb5ff766c7061af0f87.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcb847036c1289cdf34aae1c73e2d29c",
+    "revision": "50ec86082245c7406bc86ec6e8483818",
     "url": "/deploiement/index.html"
   },
   {
-    "revision": "3911a2cb62e9e4b8b9c3",
-    "url": "/deploiement/static/css/main.f1d86555.chunk.css"
+    "revision": "40fad33a9b6a5e1178ab",
+    "url": "/deploiement/static/css/main.3a4dc651.chunk.css"
   },
   {
     "revision": "7fbb420e4c602e6588fd",
     "url": "/deploiement/static/js/2.ace05756.chunk.js"
   },
   {
-    "revision": "3911a2cb62e9e4b8b9c3",
-    "url": "/deploiement/static/js/main.024f2903.chunk.js"
+    "revision": "40fad33a9b6a5e1178ab",
+    "url": "/deploiement/static/js/main.2e430eba.chunk.js"
   },
   {
     "revision": "bd7e56c0c1e6c34abf47",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deploiement/precache-manifest.00b1ce753eeb745793da6251b478b1e5.js"
+  "/deploiement/precache-manifest.a13f55037fab5fb5ff766c7061af0f87.js"
 );
 
 self.addEventListener('message', (event) => {
